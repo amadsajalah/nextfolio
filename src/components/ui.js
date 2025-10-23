6620648178ea9103e56987056c40952ef4e02fa9
AMD <dhafaachmad2@gmail.com>
2025-10-23 20:42:18 +0700

    Update ui.js

--- src/components/ui.js
+++ src/components/ui.js
@@ -4,5 +4,6 @@ import Footer from './Footer'
 import Section1 from './sections/Section1'
 import Section2 from './sections/Section2'
 import Section3 from './sections/Section3'
+import OnePieceCard from './OnePieceCard' 
 
-export { SocialIcon, Navbar, Section1, Section2, Section3, Footer }
+export { SocialIcon, Navbar, Section1, Section2, Section3, Footer, OnePieceCard}
